d23a544897790c180878cca5931004a2d24d7f93
Payden Hoskins <paydenhoskins@gmail.com>
2025-03-23 17:20:07 -0600

    Added Code

--- RollofTheDice/RollofTheDice.Designer.vb
+++ RollofTheDice/RollofTheDice.Designer.vb
@@ -22,10 +22,71 @@ Partial Class RollofTheDice
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.DiceListBox = New System.Windows.Forms.ListBox()
+        Me.RollButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.EndButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'DiceListBox
+        '
+        Me.DiceListBox.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.DiceListBox.Font = New System.Drawing.Font("Consolas", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DiceListBox.FormattingEnabled = True
+        Me.DiceListBox.ItemHeight = 19
+        Me.DiceListBox.Location = New System.Drawing.Point(5, 15)
+        Me.DiceListBox.Margin = New System.Windows.Forms.Padding(100, 3, 3, 3)
+        Me.DiceListBox.Name = "DiceListBox"
+        Me.DiceListBox.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.DiceListBox.Size = New System.Drawing.Size(783, 251)
+        Me.DiceListBox.TabIndex = 0
+        '
+        'RollButton
+        '
+        Me.RollButton.Location = New System.Drawing.Point(5, 324)
+        Me.RollButton.Name = "RollButton"
+        Me.RollButton.Size = New System.Drawing.Size(260, 105)
+        Me.RollButton.TabIndex = 1
+        Me.RollButton.Text = "Roll"
+        Me.RollButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Location = New System.Drawing.Point(271, 324)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(260, 105)
+        Me.ClearButton.TabIndex = 2
+        Me.ClearButton.Text = "Clear"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'EndButton
+        '
+        Me.EndButton.Location = New System.Drawing.Point(537, 324)
+        Me.EndButton.Name = "EndButton"
+        Me.EndButton.Size = New System.Drawing.Size(260, 105)
+        Me.EndButton.TabIndex = 3
+        Me.EndButton.Text = "End"
+        Me.EndButton.UseVisualStyleBackColor = True
+        '
+        'RollofTheDice
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.EndButton)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.RollButton)
+        Me.Controls.Add(Me.DiceListBox)
+        Me.Name = "RollofTheDice"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents DiceListBox As ListBox
+    Friend WithEvents RollButton As Button
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents EndButton As Button
 End Class
--- RollofTheDice/RollofTheDice.vb
+++ RollofTheDice/RollofTheDice.vb
@@ -1,3 +1,56 @@
-﻿Public Class RollofTheDice
+﻿'Payden Hoskins
+'RCET2265
+'Spring 2026
+'RollofTheDice
+'https://github.com/PaydenHoskins/RollofTheDice.git
 
+Option Compare Text
+Option Explicit On
+Option Strict On
+
+Public Class RollofTheDice
+    Sub DiceCounter()
+        'Test the randomness of the dice rolls
+        Dim RollCounter(12) As Integer
+        Dim headerRow
+        Dim dataRow
+        For i = 1 To 1000
+            RollCounter(RandomNumber(1, 12)) += 1
+        Next
+        DiceListBox.Items.Add("Roll of the Dice".PadLeft(49))
+        DiceListBox.Items.Add(StrDup(86, "-"))
+        For i = 2 To UBound(RollCounter)
+            headerRow &= ($"{CStr(i).PadLeft(4)}  |")
+        Next
+        DiceListBox.Items.Add(vbNewLine)
+        For i = 2 To UBound(RollCounter)
+            dataRow &= ($"{CStr(RollCounter(i)).PadLeft(4)}  |")
+        Next
+        DiceListBox.Items.Add($"   {headerRow}")
+        DiceListBox.Items.Add(StrDup(86, "-"))
+        DiceListBox.Items.Add($"   {dataRow}")
+        DiceListBox.Items.Add(StrDup(86, "-"))
+        DiceListBox.Items.Add(vbNewLine)
+    End Sub
+
+    Function RandomNumber(Min As Integer, Max As Integer) As Integer
+        'Math.
+        Dim Dice As Single
+        Randomize()
+        Dice = Rnd()
+        Dice *= Max - Min
+        Dice += Min
+        Return CInt(Math.Ceiling(Dice))
+    End Function
+    Private Sub RollButton_Click(sender As Object, e As EventArgs) Handles RollButton.Click
+        'Call the sub to display the count of the dice
+        DiceCounter()
+    End Sub
+
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        DiceListBox.Items.Clear()
+    End Sub
+    Private Sub EndButton_Click(sender As Object, e As EventArgs) Handles EndButton.Click
+        End
+    End Sub
 End Class
